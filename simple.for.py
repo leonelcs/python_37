2ebaa6f8419be2291c5100b8eb510b68681a0e39
Leonel Candido da Silva <leonel.da.silva@capgemini.com>
2019-08-22 11:31:38 +0200

    more samples and tests

--- simple.for.py
+++ simple.for.py
@@ -1,3 +1,27 @@
 surnames = ['Rivest', 'Shamir', 'Adleman'] 
 for position in range(len(surnames)): 
     print(position, surnames[position]) 
+
+for position, surname in enumerate(surnames): 
+    print(position, surname)
+
+for position, surname in enumerate(sorted(surnames)): 
+    print(position, surname)
+
+n = 39
+remainders = []
+while n > 0:
+    remainder = n % 2  # remainder of division by 2
+    remainders.insert(0, remainder)  # we keep track of remainders
+    n //= 2  # we divide n by 2
+
+print(remainders)
+
+people = ['Conrad', 'Deepak', 'Heinrich', 'Tom']
+ages = [29, 30, 34, 36]
+position = 0
+while position < len(people):
+    person = people[position]
+    age = ages[position]
+    print(person, age)
+    position += 1
