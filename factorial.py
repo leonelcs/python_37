0f3df2aed56b71d67c81e59ac05def6afb88c6f6
Leonel Candido da Silva <leonel.da.silva@capgemini.com>
2019-08-23 19:00:32 +0200

    documentation

--- factorial.py
+++ factorial.py
@@ -9,7 +9,16 @@ from operator import mul
 
 # f5 = factorial(5)  # f5 = 120
 
-def imprime(x, y):
-    return "x " + str(x) + " y " + str(y)
+# def imprime(x, y):
+#     return "x " + str(x) + " y " + str(y)
 
-print(reduce(imprime, range(1,6), 10))
+print(reduce(lambda x, y: x*y, range(1,6), 1))
+
+def factorial(n):
+    if n in (0,1):
+        return 1
+    else:
+        return factorial(n-1) * n
+
+f5 = factorial(5)  # f5 = 120
+print("Resultado do factorial: ", f5)
